3d78ef3455d707a3dcb9981fd16b833b692d842b
Rakshith R Nayak <rakshithnayak21@gmail.com>
2021-08-14 01:51:58 +0530

    Updates

---
+++ precache-manifest.9145f4baeb4ea694e47a0dba131073ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838a67f75274bf7815e1e381c128afdf",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "599f727a25224ca4cb61",
+    "url": "/Keeper-App/static/js/2.14ebd957.chunk.js"
+  },
+  {
+    "revision": "eaabbe8bd135b9b76e42",
+    "url": "/Keeper-App/static/js/main.587e3255.chunk.js"
+  },
+  {
+    "revision": "660c3a2d9cc725da8ecd",
+    "url": "/Keeper-App/static/js/runtime-main.78711218.js"
+  }
+]);
